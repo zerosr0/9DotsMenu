5d95617ac26d9ce09401988c94f8fe1e65413053
zerosr0 <sinter3@naver.com>
2024-02-15 17:30:52 +0900

    메뉴 클릭하면 크기가 확장되면서 9개 메뉴아이콘 생성

---
+++ main.js
@@ -0,0 +1,5 @@
+let navigation = document.querySelector(".navigation");
+let menuToggle = document.querySelector(".menuToggle");
+menuToggle.onclick = function () {
+  navigation.classList.toggle("active");
+}
