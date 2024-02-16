2dd03cd4d58feec4cf0b990180b4224163e78493
zerosr0 <sinter3@naver.com>
2024-02-16 11:36:04 +0900

    main.js 삭제, html파일에 삽입

--- main.js
+++
@@ -1,5 +0,0 @@
-let navigation = document.querySelector(".navigation");
-let menuToggle = document.querySelector(".menuToggle");
-menuToggle.onclick = function () {
-  navigation.classList.toggle("active");
-}
